780d1f071c43ea6026e8de2cc6c005c4b555edf2
Oussama-El-Amrani <96589855+Oussama-El-Amrani@users.noreply.github.com>
2023-04-01 12:20:56 +0000

    initial commit

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -36,6 +36,7 @@ const FeatureList = [
 ];
 
 function Feature({Svg, title, description}) {
+  
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -54,7 +55,8 @@ export default function HomepageFeatures() {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
+          {
+          FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
         </div>
